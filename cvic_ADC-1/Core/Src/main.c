1488f178fb622fe5279afa64d3bd1d9ec35fc311
xcicha04 <augustin.cich@gmail.com>
2020-03-09 19:16:19 +0100

    cvic_ADC - hotova kumulace

--- cvic_ADC-1/Core/Src/main.c
+++ cvic_ADC-1/Core/Src/main.c
@@ -33,6 +33,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define ADC_Q 12
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -57,11 +58,15 @@ static void MX_ADC_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-static volatile uint32_t raw_pot;
+volatile uint32_t raw_pot;
+volatile uint32_t avg_pot = 0;
 
 	void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 	{
-		raw_pot = HAL_ADC_GetValue(hadc);
+		raw_pot = avg_pot >> ADC_Q;
+		avg_pot -= raw_pot;
+		avg_pot += HAL_ADC_GetValue(hadc);
+
 	}
 /* USER CODE END 0 */
 
